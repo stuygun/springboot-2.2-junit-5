ef1e5a06734ee896a402b6b914c712d7091def3e
Serhan Tuygun <tuygun@gmail.com>
2019-11-07 13:44:36 +0000

    The missing TODO implementations

--- src/test/java/com/viooh/sandbox/todo/controller/TaskControllerTest.java
+++ src/test/java/com/viooh/sandbox/todo/controller/TaskControllerTest.java
@@ -43,19 +43,121 @@ public class TaskControllerTest {
         );
     }
 
-    //TODO: implement Get by Id test case
+    @Test
+    @DisplayName("Get By Id successful")
+    public void getById(){
+        Task task_from_controller = Task.builder().userId(222).title("Task from controller").build();
+        ResponseEntity<Task> taskResponseEntity = this.restTemplate.postForEntity(getUri(), task_from_controller, Task.class);
+        assertAll (
+                () -> assertNotNull(taskResponseEntity),
+                () -> assertThat(taskResponseEntity.getStatusCode(), is(equalTo(HttpStatus.OK))),
+                () -> assertNotNull(taskResponseEntity.getBody().getId()),
+                () -> assertThat(taskResponseEntity.getBody().getTitle(), is(equalTo("Task from controller"))),
+                () -> assertThat(taskResponseEntity.getBody().getUserId(), is(equalTo(222)))
+        );
 
-    //TODO: implement Get by Id non-existing task test case
+        ResponseEntity<Task> getByIdTask = this.restTemplate.getForEntity(getUri() + "/" + taskResponseEntity.getBody().getId(), Task.class);
+        assertAll (
+                () -> assertNotNull(getByIdTask),
+                () -> assertThat(getByIdTask.getStatusCode(), is(equalTo(HttpStatus.OK))),
+                () -> assertNotNull(getByIdTask.getBody().getId()),
+                () -> assertThat(getByIdTask.getBody().getTitle(), is(equalTo("Task from controller"))),
+                () -> assertThat(getByIdTask.getBody().getUserId(), is(equalTo(222)))
+        );
+    }
 
-    //TODO: implement Created Close Task test case
+    @Test
+    @DisplayName("Get By Id non-existing task")
+    public void getByIdNonExistingTask(){
+        ResponseEntity<String> errorResponse = this.restTemplate.getForEntity(getUri() + "/9999", String.class);
+        assertAll (
+                () -> assertNotNull(errorResponse),
+                () -> assertThat(errorResponse.getStatusCode(), is(equalTo(HttpStatus.NOT_FOUND)))
+        );
+    }
 
-    //TODO: implement Close Open Task test case
+    @DisplayName("Create Close Task")
+    @Test
+    public void createCloseTask(){
+        Task task_from_controller = Task.builder().userId(333).title("Task from controller").completed(true).build();
+        ResponseEntity<String> errorResponse = this.restTemplate.postForEntity(getUri(), task_from_controller, String.class);
+        assertAll (
+                () -> assertNotNull(errorResponse),
+                () -> assertThat(errorResponse.getStatusCode(), is(equalTo(HttpStatus.INTERNAL_SERVER_ERROR))),
+                () -> assertThat(errorResponse.getBody(), is(equalTo("An error occurred!")))
+        );
+    }
 
-    //TODO: implement Close Closed Task test case
+    @Test
+    @DisplayName("Close Open Task successful")
+    public void closeOpenTask(){
+        Task task_from_controller = Task.builder().userId(444).title("Task from controller").build();
+        ResponseEntity<Task> taskResponseEntity = this.restTemplate.postForEntity(getUri(), task_from_controller, Task.class);
+        assertAll (
+                () -> assertNotNull(taskResponseEntity),
+                () -> assertThat(taskResponseEntity.getStatusCode(), is(equalTo(HttpStatus.OK))),
+                () -> assertNotNull(taskResponseEntity.getBody().getId()),
+                () -> assertThat(taskResponseEntity.getBody().isCompleted(), is(equalTo(false))),
+                () -> assertThat(taskResponseEntity.getBody().getTitle(), is(equalTo("Task from controller"))),
+                () -> assertThat(taskResponseEntity.getBody().getUserId(), is(equalTo(444)))
+        );
 
-    //TODO: implement Close non-existing task test case
+        ResponseEntity<Task> getByIdTask = this.restTemplate.getForEntity(getUri() + "/" + taskResponseEntity.getBody().getId() + "/close", Task.class);
+        assertAll (
+                () -> assertNotNull(getByIdTask),
+                () -> assertThat(getByIdTask.getStatusCode(), is(equalTo(HttpStatus.OK))),
+                () -> assertThat(getByIdTask.getBody().isCompleted(), is(equalTo(true))),
+                () -> assertNotNull(getByIdTask.getBody().getId()),
+                () -> assertThat(getByIdTask.getBody().getTitle(), is(equalTo("Task from controller"))),
+                () -> assertThat(getByIdTask.getBody().getUserId(), is(equalTo(444)))
+        );
+    }
+
+    @Test
+    @DisplayName("Close Closed Task successful")
+    public void closeClosedTask(){
+        Task task_from_controller = Task.builder().userId(555).title("Task from controller").build();
+        ResponseEntity<Task> taskResponseEntity = this.restTemplate.postForEntity(getUri(), task_from_controller, Task.class);
+        assertAll (
+                () -> assertNotNull(taskResponseEntity),
+                () -> assertThat(taskResponseEntity.getStatusCode(), is(equalTo(HttpStatus.OK))),
+                () -> assertNotNull(taskResponseEntity.getBody().getId()),
+                () -> assertThat(taskResponseEntity.getBody().isCompleted(), is(equalTo(false))),
+                () -> assertThat(taskResponseEntity.getBody().getTitle(), is(equalTo("Task from controller"))),
+                () -> assertThat(taskResponseEntity.getBody().getUserId(), is(equalTo(555)))
+        );
+
+        ResponseEntity<Task> getByIdTask = this.restTemplate.getForEntity(getUri() + "/" + taskResponseEntity.getBody().getId() + "/close", Task.class);
+        assertAll (
+                () -> assertNotNull(getByIdTask),
+                () -> assertThat(getByIdTask.getStatusCode(), is(equalTo(HttpStatus.OK))),
+                () -> assertThat(getByIdTask.getBody().isCompleted(), is(equalTo(true))),
+                () -> assertNotNull(getByIdTask.getBody().getId()),
+                () -> assertThat(getByIdTask.getBody().getTitle(), is(equalTo("Task from controller"))),
+                () -> assertThat(getByIdTask.getBody().getUserId(), is(equalTo(555)))
+        );
+
+        ResponseEntity<String> errorResponse = this.restTemplate.getForEntity(getUri() + "/" + taskResponseEntity.getBody().getId() + "/close", String.class);
+        assertAll (
+                () -> assertNotNull(errorResponse),
+                () -> assertThat(errorResponse.getStatusCode(), is(equalTo(HttpStatus.INTERNAL_SERVER_ERROR))),
+                () -> assertThat(errorResponse.getBody(), is(equalTo("An error occurred!")))
+        );
+    }
+
+    @Test
+    @DisplayName("Get By Id non-existing task")
+    public void closeNonExistingTask(){
+        ResponseEntity<String> errorResponse = this.restTemplate.getForEntity(getUri() + "/9999/close", String.class);
+        assertAll (
+                () -> assertNotNull(errorResponse),
+                () -> assertThat(errorResponse.getStatusCode(), is(equalTo(HttpStatus.INTERNAL_SERVER_ERROR))),
+                () -> assertThat(errorResponse.getBody(), is(equalTo("An error occurred!")))
+        );
+    }
 
     private String getUri() {
         return "http://localhost:" + port + contextPath + "tasks";
     }
 }
+
